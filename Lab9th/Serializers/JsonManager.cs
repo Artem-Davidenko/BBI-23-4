c96f02b53b83ea66c17b2506e243177fa1e237ed
Davidenko_Artem <159883412+Artem-Davidenko@users.noreply.github.com>
2024-05-27 20:55:33 +0300

    Lab9 done

---
+++ Lab9th/Serializers/JsonManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LastLangVersion.Serializers
+{
+    public class JsonManager : Serial
+    {
+        public override void Write<T>(T obj, string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize(fs, obj);
+            }
+        }
+        public override T Read<T>(string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                return JsonSerializer.Deserialize<T>(fs);
+            }
+        }
+    }
+}
---
+++ Lab9th/Serializers/Serial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastLangVersion.Serializers
+{
+    public abstract class Serial
+    {
+        public abstract void Write<T>(T obj, string filePath);
+        public abstract T Read<T>(string filePath);
+    }
+}
